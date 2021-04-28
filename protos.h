43259a9c45829154b3fbeb2541c655d241e59676
Jason Bacon <bacon@coral.acadix.biz>
2021-04-28 10:17:06 -0500

    find_start_positions(): Switch to low-level I/O Noticeable improvement in speed

--- protos.h
+++ protos.h
@@ -1,4 +1,4 @@
 /* cut-and-run.c */
 int main(int argc, char *argv[]);
-long *find_start_positions(FILE *infile, unsigned thread_count);
-int spawn_processes(char *filename, long start_positions[], unsigned thread_count);
+long *find_start_positions(int infile, unsigned thread_count);
+int spawn_processes(char *filename, char *cmd, char *out_filename, long start_positions[], unsigned thread_count);
